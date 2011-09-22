e1fc03dfa553f810a0767ab944675a1bf28cdc8b
Jérôme Lebel <jerome@fotonauts.com>
2011-09-22 03:19:42 +0200

    starting to convert objects into json

--- MODHelper.m
+++ MODHelper.m
@@ -8,12 +8,117 @@
 
 #import "MODHelper.h"
 #import "MODObjectId.h"
+#import "NSString+Extras.h"
 
 @interface MODHelper()
 + (NSMutableDictionary *)convertForOutlineWithValue:(id)dataValue dataKey:(NSString *)dataKey;
 @end
 
+static void convertValueToJson(NSMutableString *result, int indent, id value, NSString *key, BOOL pretty);
+
+static void addIdent(NSMutableString *result, int indent)
+{
+    int ii = 0;
+    
+    while (ii < indent) {
+        [result appendString:@"  "];
+        ii++;
+    }
+}
+
+static void convertDictionaryToJson(NSMutableString *result, int indent, NSDictionary *value, BOOL pretty)
+{
+    BOOL first = YES;
+    
+    [result appendString:@"{"];
+    if (pretty) {
+        [result appendString:@"\n"];
+    }
+    for (NSString *key in [[value allKeys] sortedArrayUsingSelector:@selector(compare:)]) {
+        if (first) {
+            first = NO;
+        } else {
+            [result appendString:@",\n"];
+        }
+        convertValueToJson(result, indent + 1, [value objectForKey:key], key, pretty);
+    }
+    if (pretty) {
+        [result appendString:@"\n"];
+        addIdent(result, indent);
+    }
+    [result appendString:@"}"];
+}
+
+static void convertArrayToJson(NSMutableString *result, int indent, NSArray *value, BOOL pretty)
+{
+    BOOL first = YES;
+    
+    [result appendString:@"["];
+    if (pretty) {
+        [result appendString:@"\n"];
+    }
+    for (id arrayValue in value) {
+        if (first) {
+            first = NO;
+        } else {
+            [result appendString:@",\n"];
+        }
+        convertValueToJson(result, indent + 1, arrayValue, nil, pretty);
+    }
+    if (pretty) {
+        [result appendString:@"\n"];
+        addIdent(result, indent);
+    }
+    [result appendString:@"]"];
+}
+
+static void convertValueToJson(NSMutableString *result, int indent, id value, NSString *key, BOOL pretty)
+{
+    if (pretty) {
+        addIdent(result, indent);
+    }
+    if (key) {
+        [result appendString:@"\""];
+        [result appendString:[key escapeQuotes]];
+        [result appendString:@"\": "];
+    }
+    if ([value isKindOfClass:[NSString class]]) {
+        [result appendString:@"\""];
+        [result appendString:[value escapeQuotes]];
+        [result appendString:@"\""];
+    } else if ([value isKindOfClass:[NSDate class]]) {
+        [result appendFormat:@"%f", [value timeIntervalSince1970] * 1000];
+    } else if ([value isKindOfClass:[NSNull class]]) {
+        [result appendString:@"null"];
+    } else if ([value isKindOfClass:[NSDictionary class]]) {
+        convertDictionaryToJson(result, indent, value, pretty);
+    } else if ([value isKindOfClass:[NSArray class]]) {
+        convertArrayToJson(result, indent, value, pretty);
+    } else if ([value isKindOfClass:[NSNumber class]]) {
+        if (strcmp([value objCType], @encode(BOOL)) == 0) {
+            if ([value boolValue]) {
+                [result appendString:@"true"];
+            } else {
+                [result appendString:@"false"];
+            }
+        } else {
+            [result appendString:[value description]];
+        }
+    } else if ([value isKindOfClass:[MODObjectId class]]) {
+        [result appendString:[value jsonValue]];
+    }
+}
+
 @implementation MODHelper
+
++ (NSString *)convertObjectToJson:(NSDictionary *)object pretty:(BOOL)pretty
+{
+    NSMutableString *result;
+    
+    result = [NSMutableString string];
+    convertDictionaryToJson(result, 0, object, pretty);
+    return result;
+}
 
 + (NSArray *)convertForOutlineWithObjects:(NSArray *)mongoObjects
 {
@@ -28,6 +133,7 @@
             dict = [NSMutableDictionary dictionary];
         }
         [dict setObject:[self convertForOutlineWithObject:object] forKey:@"child"];
+        [dict setObject:[self convertObjectToJson:object pretty:YES] forKey:@"beautified"];
         [result addObject:dict];
     }
     return result;
@@ -110,9 +216,11 @@
     }
     if (value) {
         result = [NSMutableDictionary dictionaryWithCapacity:4];
-        [result setValue:value forKey:@"value"];
-        [result setValue:dataKey forKey:@"name"];
-        [result setValue:type forKey:@"type"];
+        [result setObject:value forKey:@"value"];
+        [result setObject:dataKey forKey:@"name"];
+        [result setObject:type forKey:@"type"];
+        //[result setObject:jsonString forKey:@"raw"];
+        //[result setObject:jsonStringb forKey:@"beautified"];
         if (child) {
             [result setValue:child forKey:@"child"];
         }

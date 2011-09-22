db9a784a0cf404136dee98e1c269268af2de1aba
Jérôme Lebel <jerome@fotonauts.com>
2011-09-22 03:19:06 +0200

    better coding style and adding -[NSString escapeQuotes]

--- NSString+Extras.m
+++ NSString+Extras.m
@@ -11,54 +11,96 @@
 
 @implementation NSString (Extras)
 
-
-+ (NSString*)stringFromResource:(NSString*)resourceName {
++ (NSString*)stringFromResource:(NSString*)resourceName
+{
     NSString *path = [[NSBundle mainBundle] pathForResource:resourceName ofType:nil];
     return [NSString stringWithContentsOfFile:path usedEncoding:nil error:nil];
 }
 
 # pragma Comparing
 
-- (BOOL)startsWithString:(NSString*)otherString {
+- (NSString *)escapeQuotes
+{
+    NSMutableString *result;
+    NSUInteger ii = 0, count = [self length];
+    
+    result = [self mutableCopy];
+    while (ii < count) {
+        if ([result characterAtIndex:ii] == '"' || [result characterAtIndex:ii] == '\\') {
+            [result insertString:@"\\" atIndex:ii];
+            ii++;
+            count++;
+        } else if ([result characterAtIndex:ii] == '\n') {
+            [result deleteCharactersInRange:NSMakeRange(ii, 1)];
+            [result insertString:@"\\n" atIndex:ii];
+            ii++;
+            count++;
+        } else if ([result characterAtIndex:ii] == '\r') {
+            [result deleteCharactersInRange:NSMakeRange(ii, 1)];
+            [result insertString:@"\\r" atIndex:ii];
+            ii++;
+            count++;
+        } else if ([result characterAtIndex:ii] == '\t') {
+            [result deleteCharactersInRange:NSMakeRange(ii, 1)];
+            [result insertString:@"\\t" atIndex:ii];
+            ii++;
+            count++;
+        }
+        ii++;
+    }
+    return [result autorelease];
+}
+
+- (BOOL)startsWithString:(NSString*)otherString
+{
     return [self rangeOfString:otherString].location == 0;
 }
 
-- (BOOL)endsWithString:(NSString*)otherString {
+- (BOOL)endsWithString:(NSString*)otherString
+{
     return [self rangeOfString:otherString].location == [self length]-[otherString length];
 }
 
-- (BOOL)isPresent {
+- (BOOL)isPresent
+{
     return ![self isEqualToString:@""];
 }
 
-- (NSComparisonResult)compareCaseInsensitive:(NSString*)other {
+- (NSComparisonResult)compareCaseInsensitive:(NSString*)other
+{
     NSString *selfString = [self lowercaseString];
     NSString *otherString = [other lowercaseString];
     return [selfString compare:otherString];
 }
 
-- (NSString*)stringByPercentEscapingCharacters:(NSString*)characters {
+- (NSString*)stringByPercentEscapingCharacters:(NSString*)characters
+{
     return [(NSString*)CFURLCreateStringByAddingPercentEscapes(NULL, (CFStringRef)self, NULL, (CFStringRef)characters, kCFStringEncodingUTF8) autorelease];
 }
 
-- (NSString*)stringByEscapingURL {
+- (NSString*)stringByEscapingURL
+{
     return [self stringByPercentEscapingCharacters:@";/?:@&=+$,"];    
 }
 
-- (NSString*)stringByUnescapingURL {
+- (NSString*)stringByUnescapingURL
+{
     return [(NSString*)CFURLCreateStringByReplacingPercentEscapes(NULL, (CFStringRef)self, CFSTR("")) autorelease];
 }
 
-- (BOOL)containsString:(NSString *)aString {
+- (BOOL)containsString:(NSString *)aString
+{
     return [self containsString:aString ignoringCase:NO];
 }
 
-- (BOOL)containsString:(NSString *)aString ignoringCase:(BOOL)flag {
+- (BOOL)containsString:(NSString *)aString ignoringCase:(BOOL)flag
+{
     unsigned mask = (flag ? NSCaseInsensitiveSearch : 0);
     return [self rangeOfString:aString options:mask].length > 0;
 }
 
-- (int)countSubstring:(NSString *)aString ignoringCase:(BOOL)flag {
+- (int)countSubstring:(NSString *)aString ignoringCase:(BOOL)flag
+{
     unsigned mask = (flag ? NSCaseInsensitiveSearch : 0);
     return [self rangeOfString:aString options:mask].length;
 }
@@ -73,7 +115,8 @@
     return [NSNull null];
 }
 
-+ (NSString*)UUIDString {
++ (NSString*)UUIDString
+{
     CFUUIDRef theUUID = CFUUIDCreate(NULL);
     CFStringRef string = CFUUIDCreateString(NULL, theUUID);
     CFRelease(theUUID);

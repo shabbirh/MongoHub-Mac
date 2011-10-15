6257d23f15552ec1462297dbea11957a202f9930
Jérôme Lebel <jerome@fotonauts.com>
2011-10-15 02:55:36 +0200

    some anoying warnings

--- QueryWindowController.m
+++ QueryWindowController.m
@@ -876,7 +876,7 @@
      	NSLog(@"doOpen we have a Cancel button");
      	return;
     } else {
-     	NSLog(@"doOpen tvarInt not equal 1 or zero = %3ld",tvarNSInteger);
+     	NSLog(@"doOpen tvarInt not equal 1 or zero = %3d",(int)tvarNSInteger);
      	return;
     } // end if
 }

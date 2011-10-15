98387554be6430c6ca27229318cf20fce03ee787
Jérôme Lebel <jerome@fotonauts.com>
2011-10-15 02:54:50 +0200

    some anoying warnings

--- AddConnectionController.m
+++ AddConnectionController.m
@@ -244,7 +244,7 @@
      	NSLog(@"doOpen we have a Cancel button");
      	return;
     } else {
-     	NSLog(@"doOpen tvarInt not equal 1 or zero = %3li",tvarNSInteger);
+     	NSLog(@"doOpen tvarInt not equal 1 or zero = %3d",(int)tvarNSInteger);
      	return;
     } // end if
 }

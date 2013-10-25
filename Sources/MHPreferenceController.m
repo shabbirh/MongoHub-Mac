8d576abfd3bb741c83c7eb0775806a83de59c4cf
Jérôme Lebel <jerome@fotonauts.com>
2013-10-25 14:22:39 +0200

    working on some preferences

---
+++ Sources/MHPreferenceController.m
@@ -0,0 +1,13 @@
+//
+//  MHPreferenceController.m
+//  MongoHub
+//
+//  Created by Jérôme Lebel on 23/10/13.
+//  Copyright (c) 2013 ThePeppersStudio.COM. All rights reserved.
+//
+
+#import "MHPreferenceController.h"
+
+@implementation MHPreferenceController
+
+@end

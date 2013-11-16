6c9f6f5b8f4f74d39419f52a9e82a3544f5f495f
Jérôme Lebel <jerome@fotonauts.com>
2013-11-16 20:35:41 +0100

    nicer script

--- create_version.sh
+++ create_version.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
-VERSION="2.5.13(107)"
+VERSION="2.5.14"
+VERSION="$1"
+echo $VERSION
 git commit -m "software update $VERSION" .
 git tag -a "$VERSION" -m "software update $VERSION"
 git push --tags

8644fcacbbfb4043e17760dc750cbf18c0c240a4
Jérôme Lebel <jerome@fotonauts.com>
2014-01-16 14:19:57 +0100

    software update 2.6b6

--- create_version.sh
+++ create_version.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 VERSION="2.5.14"
 VERSION="$1"
-echo $VERSION
+if [ "$VERSION" = "" ] ; then
+  echo "Need the version as the parameter"
+  exit 1
+fi
 git commit -m "software update $VERSION" .
 git push
 git tag -a "$VERSION" -m "software update $VERSION"

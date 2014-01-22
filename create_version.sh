f23f85902afc0febb7de13ee561e16b171925779
Joseph Price <joseph.price@huffingtonpost.com>
2014-01-21 22:09:12 -0500

    Exit create version script if no version provided

--- create_version.sh
+++ create_version.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
+
+set -eu
+
+usage() {
+  echo "Usage: $(basename $0) VERSION"
+}
+
 VERSION="2.5.14"
-VERSION="$1"
-echo $VERSION
+VERSION="${1:?$(usage)}"
+
 git commit -m "software update $VERSION" .
 git push
 git tag -a "$VERSION" -m "software update $VERSION"
